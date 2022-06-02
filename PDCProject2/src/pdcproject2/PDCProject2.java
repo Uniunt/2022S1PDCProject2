5e1cca61a6acf7c903c8b074369d2bc14be225a1
Uniunt <Uniunt@26.26.26.1>
2022-06-03 00:55:02 +0800

    Hello GitHub

---
+++ PDCProject2/src/pdcproject2/PDCProject2.java
@@ -0,0 +1,16 @@
+package pdcproject2;
+
+/**
+ *
+ * @author Uniunt
+ */
+public class PDCProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
